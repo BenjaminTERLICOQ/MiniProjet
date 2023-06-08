87547b3db6e09976f4c57c3a5246dc1fd821f839
BenjaminTERLICOQ <131340032+BenjaminTERLICOQ@users.noreply.github.com>
2023-06-08 11:24:26 +0200

    scriptrecherche

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function recherche(){
     let searchbar = document.getElementById("searchbar").value;
+    searchbar = searchbar.toLowerCase();
+    let x = document.getElementsByClassName("magasin");
+
+    for (i = 0 ; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(searchbar)){
+            x[i].style.display ="none";
+
+        }
+       else {
+        x[i].style.display = "list-item";
+       }
+    }
 }
